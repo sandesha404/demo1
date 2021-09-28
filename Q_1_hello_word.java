819331d8dc4354809fe7977c4e54c408f43adb52
sandesha404 <91174871+sandesha404@users.noreply.github.com>
2021-09-28 12:15:49 +0530

    My first file upload change

---
+++ Q_1_hello_word.java
@@ -0,0 +1,17 @@
+package assignment_1;
+
+class Q_1_hello_word 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		{
+			System.out.println("hello word");
+		}
+		{
+			System.out.println("Sandesh Shinde ");
+		}
+	}
+
+}
