112aed17ef3023e5630f0d857559a1749408075c
sandesha404 <91174871+sandesha404@users.noreply.github.com>
2021-09-28 12:27:41 +0530

    my upload branch change

---
+++ Q_2_Swap_no.java
@@ -0,0 +1,22 @@
+package assignment_1;
+
+class Q_2_Swap_no
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int temp;
+		{
+			System.out.println("before swaping print value in temp = " +a+"  "+b );
+		}
+		temp=a;
+		a=b;
+		b=temp;
+		{
+			System.out.println("after swaping print value in temp = " +a+"  " +b  );
+		}
+	}
+
+}
